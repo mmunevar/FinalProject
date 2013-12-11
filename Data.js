c8a40f655e209dfe884ea8d060b1de466f9b6855
Munevar Miguel <mune0006@algonquinlive.com>
2013-12-11 13:21:22 -0500

    first commit

---
+++ Data.js
@@ -0,0 +1,36 @@
+{"campusservices":[
+
+{"service":"Facilities","campusdetail":
+[{"facility":"Theatre","place":"Blg E"},
+{"facility":"Gym","place":"Blg A"},
+{"facility":"Library","place":"Blg C"}
+]
+},
+{"service":"Health Services ","campusdetail":
+[{"facility":"Medical Centre","place":"Blg A"},
+{"facility":"Dental Centre","place":"Residence"},
+{"facility":"Massage & Therapy","place":"Blg C"}
+]
+},
+{"service":"IT Stores & Services","campusdetail":
+[{"facility":"Connections","place":"Blg E"},
+{"facility":"TheBookStore","place":"Blg E"},
+{"facility":"Technology Store'  Bytes","place":"Blg E"}
+]
+},
+{"service":"Peer Tutoring","campusdetail":
+[{"facility":" Math 101","place":"Blg D"},
+{"facility":" Counseling","place":"Blg H"},
+{"facility":" 1-2-1 Tutor'  Bytes","place":"Blg D"},
+{"facility":"  Group Tutoring","place":"Blg A"}
+]
+},
+{"service":"Food and Beverage","campusdetail":
+[{"facility":" Market Place","place":"Blg C"},
+{"facility":" 35th Street Market Café","place":"Blg H"},
+{"facility":" Bits N’ Bytes","place":"Blg A"},
+{"facility":" The Portable Feast","place":"Blg C"}
+]
+}
+]
+}
